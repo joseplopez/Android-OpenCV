6c8b6b827a2656fc9d89bb52dd1a5ddb58db59e5
Josep López <jple@gft.com>
2016-10-22 11:11:35 +0200

    feat: Added function to detect squares via camera stream

---
+++ app/src/main/java/com/udacity/josep/opencv/MainActivity.java
@@ -0,0 +1,260 @@
+package com.udacity.josep.opencv;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.os.Build.VERSION_CODES.N;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    static{ System.loadLibrary("opencv_java3"); }
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        textView = (TextView) findViewById(R.id.total);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    List<MatOfPoint> squares = new ArrayList<MatOfPoint>();
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        //return inputFrame.rgba();
+        if (Math.random()>0.80) {
+            findSquares(inputFrame.rgba().clone(),squares);
+        }
+        Mat image = inputFrame.rgba();
+        Handler mainHandler = new Handler(this.getMainLooper());
+        Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {textView.setText("Total squares: " + squares.size());}
+        };
+        mainHandler.post(myRunnable);
+        Imgproc.drawContours(image, squares, -1, new Scalar(0,0,255));
+        return image;
+    }
+
+    // helper function:
+    // finds a cosine of angle between vectors
+    // from pt0->pt1 and from pt0->pt2
+    double angle(Point pt1, Point pt2, Point pt0 ) {
+        double dx1 = pt1.x - pt0.x;
+        double dy1 = pt1.y - pt0.y;
+        double dx2 = pt2.x - pt0.x;
+        double dy2 = pt2.y - pt0.y;
+        return (dx1*dx2 + dy1*dy2)/Math.sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+    }
+
+    // returns sequence of squares detected on the image.
+    // the sequence is stored in the specified memory storage
+    void findSquares( Mat image, List<MatOfPoint> squares )
+    {
+
+        squares.clear();
+
+        Mat smallerImg=new Mat(new Size(image.width()/2, image.height()/2),image.type());
+
+        Mat gray=new Mat(image.size(),image.type());
+
+        Mat gray0=new Mat(image.size(), CvType.CV_8U);
+
+        // down-scale and upscale the image to filter out the noise
+        Imgproc.pyrDown(image, smallerImg, smallerImg.size());
+        Imgproc.pyrUp(smallerImg, image, image.size());
+
+        // find squares in every color plane of the image
+        for( int c = 0; c < 3; c++ )
+        {
+
+            extractChannel(image, gray, c);
+
+            // try several threshold levels
+            for( int l = 1; l < N; l++ )
+            {
+                //Cany removed... Didn't work so well
+
+
+                Imgproc.threshold(gray, gray0, (l+1)*255/N, 255, Imgproc.THRESH_BINARY);
+
+
+                List<MatOfPoint> contours=new ArrayList<MatOfPoint>();
+
+                // find contours and store them all as a list
+                Imgproc.findContours(gray0, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                MatOfPoint approx=new MatOfPoint();
+
+                // test each contour
+                for( int i = 0; i < contours.size(); i++ )
+                {
+
+                    // approximate contour with accuracy proportional
+                    // to the contour perimeter
+                    approx = approxPolyDP(contours.get(i),  Imgproc.arcLength(new MatOfPoint2f(contours.get(i).toArray()), true)*0.02, true);
+
+
+                    // square contours should have 4 vertices after approximation
+                    // relatively large area (to filter out noisy contours)
+                    // and be convex.
+                    // Note: absolute value of an area is used because
+                    // area may be positive or negative - in accordance with the
+                    // contour orientation
+
+                    if( approx.toArray().length == 4 &&
+                            Math.abs(Imgproc.contourArea(approx)) > 1000 &&
+                            Imgproc.isContourConvex(approx) )
+                    {
+                        double maxCosine = 0;
+
+                        for( int j = 2; j < 5; j++ )
+                        {
+                            // find the maximum cosine of the angle between joint edges
+                            double cosine = Math.abs(angle(approx.toArray()[j%4], approx.toArray()[j-2], approx.toArray()[j-1]));
+                            maxCosine = Math.max(maxCosine, cosine);
+                        }
+
+                        // if cosines of all angles are small
+                        // (all angles are ~90 degree) then write quandrange
+                        // vertices to resultant sequence
+                        if( maxCosine < 0.3 )
+                            squares.add(approx);
+                    }
+                }
+            }
+        }
+    }
+
+    void extractChannel(Mat source, Mat out, int channelNum) {
+        List<Mat> sourceChannels=new ArrayList<Mat>();
+        List<Mat> outChannel=new ArrayList<Mat>();
+
+        Core.split(source, sourceChannels);
+
+        outChannel.add(new Mat(sourceChannels.get(0).size(),sourceChannels.get(0).type()));
+
+        Core.mixChannels(sourceChannels, outChannel, new MatOfInt(channelNum,0));
+
+        Core.merge(outChannel, out);
+    }
+
+    MatOfPoint approxPolyDP(MatOfPoint curve, double epsilon, boolean closed) {
+        MatOfPoint2f tempMat=new MatOfPoint2f();
+
+        Imgproc.approxPolyDP(new MatOfPoint2f(curve.toArray()), tempMat, epsilon, closed);
+
+        return new MatOfPoint(tempMat.toArray());
+    }
+}
